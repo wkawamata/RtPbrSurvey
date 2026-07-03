e6465ea2b7ad90f21f9f006aaa24399517db13f2
Wataru Kawamata <wkawamata0323@gmail.com>
2026-07-04 08:40:17 +0900

    Refactor: Extract Shared/Error.h from DXSampleHelper.h
    Move error handling (HrToString, HrException, SAFE_RELEASE,
    ThrowIfFailed) to new Shared/Error.h.
    DXSampleHelper.h includes Shared/Error.h for backward compat.

--- DXSampleHelper.h
+++ DXSampleHelper.h
@@ -11,45 +11,13 @@
 
 #pragma once
 #include "Platform/FileIO.h"
-#include <stdexcept>
+#include "Shared/Error.h"
 
 // Note that while ComPtr is used to manage the lifetime of resources on the CPU,
 // it has no understanding of the lifetime of resources on the GPU. Apps must account
 // for the GPU lifetime of resources to avoid destroying objects that may still be
 // referenced by the GPU.
 using Microsoft::WRL::ComPtr;
-
-inline std::string HrToString(HRESULT hr)
-{
-    char s_str[64] = {};
-    sprintf_s(s_str, "HRESULT of 0x%08X", static_cast<UINT>(hr));
-    return std::string(s_str);
-}
-
-class HrException : public std::runtime_error
-{
-public:
-    HrException(HRESULT hr) : std::runtime_error(HrToString(hr)), m_hr(hr) {}
-    HRESULT Error() const
-    {
-        return m_hr;
-    }
-
-private:
-    const HRESULT m_hr;
-};
-
-#define SAFE_RELEASE(p)                                                                                                \
-    if (p)                                                                                                             \
-    (p)->Release()
-
-inline void ThrowIfFailed(HRESULT hr)
-{
-    if (FAILED(hr))
-    {
-        throw HrException(hr);
-    }
-}
 
 // Assign a name to the object to aid with debugging.
 #if defined(_DEBUG) || defined(DBG)
---
+++ Shared/Error.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <windows.h>
+#include <stdexcept>
+#include <string>
+
+inline std::string HrToString(HRESULT hr)
+{
+    char s_str[64] = {};
+    sprintf_s(s_str, "HRESULT of 0x%08X", static_cast<UINT>(hr));
+    return std::string(s_str);
+}
+
+class HrException : public std::runtime_error
+{
+public:
+    HrException(HRESULT hr) : std::runtime_error(HrToString(hr)), m_hr(hr) {}
+    HRESULT Error() const
+    {
+        return m_hr;
+    }
+
+private:
+    const HRESULT m_hr;
+};
+
+#define SAFE_RELEASE(p)                                                                                                \
+    if (p)                                                                                                             \
+    (p)->Release()
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+    if (FAILED(hr))
+    {
+        throw HrException(hr);
+    }
+}
