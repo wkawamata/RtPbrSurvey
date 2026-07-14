2d5f2eda9f57310b06752f21c38157f3614d82a0
Wataru Kawamata <wkawamata0323@gmail.com>
2026-07-14 13:02:57 +0900

    Use PBR direct lighting for reflection hits

--- Shaders/shaders_ReflectionRayHitDebug.hlsl
+++ Shaders/shaders_ReflectionRayHitDebug.hlsl
@@ -81,6 +81,50 @@ float3 ReconstructWorldPosition(float2 uv, float depth)
     return worldPos.xyz / worldPos.w;
 }
 
+float DistributionGGX(float ndoth, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float denom = ndoth * ndoth * (a2 - 1.0) + 1.0;
+    return a2 / max(PI * denom * denom, 0.000001);
+}
+
+float GeometrySchlickGGX(float ndotx, float roughness)
+{
+    float r = roughness + 1.0;
+    float k = (r * r) / 8.0;
+    return ndotx / max(ndotx * (1.0 - k) + k, 0.000001);
+}
+
+float GeometrySmith(float ndotv, float ndotl, float roughness)
+{
+    return GeometrySchlickGGX(ndotv, roughness) * GeometrySchlickGGX(ndotl, roughness);
+}
+
+float3 FresnelSchlick(float cosTheta, float3 f0)
+{
+    return f0 + (1.0 - f0) * pow(saturate(1.0 - cosTheta), 5.0);
+}
+
+float3 ComputeDirectRadiance(float3 albedo, float metallic, float roughness, float3 normal, float3 viewDir)
+{
+    float3 lightDir = normalize(lightDirection);
+    float3 halfDir = normalize(lightDir + viewDir);
+    float ndotl = saturate(dot(normal, lightDir));
+    float ndotv = saturate(dot(normal, viewDir));
+    float ndoth = saturate(dot(normal, halfDir));
+    float vdoth = saturate(dot(viewDir, halfDir));
+
+    float3 f0 = lerp(float3(0.04, 0.04, 0.04), albedo, metallic);
+    float3 fresnel = FresnelSchlick(vdoth, f0);
+    float distribution = DistributionGGX(ndoth, roughness);
+    float geometry = GeometrySmith(ndotv, ndotl, roughness);
+    float3 specularBrdf = distribution * geometry * fresnel / max(4.0 * ndotv * ndotl, 0.0001);
+    float3 diffuseBrdf = (1.0 - fresnel) * (1.0 - metallic) * albedo / PI;
+
+    return (diffuseBrdf + specularBrdf) * lightColor * diffuseIntensity * ndotl * directLightEnabled;
+}
+
 FullscreenVSOutput VSMain(uint vertexId : SV_VertexID)
 {
     return FullscreenTriangleVS(vertexId);
@@ -171,8 +215,9 @@ float4 PSMain(FullscreenVSOutput input) : SV_TARGET
         float distanceFade = saturate(1.0 - hitDistance / max(contributionMaxDistance, 0.001));
         float strength = hitFlag * distanceFade * (1.0 - visibleRoughness) * contributionIntensity;
 
-        float ndotl = saturate(dot(hitNormal, normalize(lightDirection)));
-        float3 directRadiance = rayColor * (1.0 - hitMetallic) * lightColor * diffuseIntensity * ndotl * directLightEnabled;
+        float3 hitViewDir = -reflectionDir;
+        float directRoughness = max(hitRoughness, 0.04);
+        float3 directRadiance = ComputeDirectRadiance(rayColor, hitMetallic, directRoughness, hitNormal, hitViewDir);
         float3 diffuseIrradiance = g_diffuseIrradianceMap.Sample(g_sampler, hitNormal).rgb;
         float3 diffuseIbl = diffuseIrradiance * rayColor * (1.0 - hitMetallic) * iblIntensity * diffuseIblEnabled / PI;
         float3 hitF0 = lerp(float3(0.04, 0.04, 0.04), rayColor, hitMetallic);
